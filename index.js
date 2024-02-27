872f59dd89a462b2faa1e5b149f565a9d12cdfd6
Sam Wallington <samuel.wallingotn@gmail.com>
2024-02-27 21:48:03 +1300

    Build out movement of the snake and begin prototyping the adding of additional segments

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ import { Segment } from './segment.js';
 const board = document.getElementById('game-board');
 
 let HEAD;
+let FOOD;
 
 // the coordinates of the food, stored to avoid placing things ontop of eachother
 let FOOD_COORDS = {
-    x: '450px',
-    y: '300px',
+    x: 450,
+    y: 300,
 };
 
 let DIRECTION = 'LEFT';
@@ -28,8 +29,10 @@ const initialise = () => {
     const food = document.createElement('div');
     food.className = 'food';
 
-    food.style.left = FOOD_COORDS.x;
-    food.style.top = FOOD_COORDS.y;
+    food.style.left = `${FOOD_COORDS.x}px`;
+    food.style.top = `${FOOD_COORDS.y}px`;
+
+    FOOD = food;
 
     // create a new segment, this handles creating the element and adding it to the board
     const snakeHead = new Segment({ x:150, y: 300 }, board);
@@ -41,22 +44,24 @@ const initialise = () => {
 
 const tick = () => {
     // snake movement
-    switch (DIRECTION) {
-        case 'UP':
-            return;
-        case 'RIGHT':
-            return;
-        case 'DOWN':
-            return;
-        case 'LEFT':
-            return;
-    }
-
+    HEAD.move(DIRECTION);
     
+    if(HEAD.coords.x === FOOD_COORDS.x && HEAD.coords.y === FOOD_COORDS.y){
+        // firstly we move the food
+        let NEW_COORDS = getRandomCoords();
+
+        while(HEAD.coords.x === NEW_COORDS.x && HEAD.coords.y === NEW_COORDS.y){
+            console.log('they match')
+            NEW_COORDS = getRandomCoords();
+        }
+
+        FOOD.style.left = `${NEW_COORDS.x}px`;
+        FOOD.style.top = `${NEW_COORDS.y}px`;
+    };
 }
 
 // around 60fps
-setInterval(tick, 16.7)
+setInterval(tick, 250)
 
 document.addEventListener('DOMContentLoaded', initialise);
 
--- segment.js
+++ segment.js
@@ -21,38 +21,72 @@ export class Segment {
 
         // append the element to the board
         board.appendChild(element);
+
+        this.element = element;
     }
 
-    set assignCoords(newCoords){
-        this.coords = newCoords;
-    }
-
-    get getCoords(){
-        return this.coords;
-    }
-
-    set appendChild(child){
-        this.child = child;
-    }
-
-    get getChild(){
-        return this.child;
-    }
-
+    /*
+     * A simple check as to whether or not this Segment is the tail of the snake.
+     */
     isTailSegment(){
         return !this.child;
     }
 
+    /**
+     * A recursive method used to add the end of the snake.
+     * 
+     * @param {Coords} previousCoords the segment that preceeds this one, used to determine position of new segment.
+     * @param board the game board for this.
+     */
+    addSegment(previousCoords, board){
+        if(!this.isTailSegment){
+            this.child.addSegment(this.coords);
+        } else {
+            newTail = new Segment({  }, board)
+        }
+    }
+
+    /**
+     * A method used to move the snake, recursively applying to it's successive segments
+     * 
+     * @param direction One of 'UP', 'RIGHT', 'LEFT', 'DOWN'
+     * @returns Moves this particular segment in the desired direction & triggers successive moves
+     */
     move(direction){
-        switch (DIRECTION) {
+        let newCoords
+
+        switch (direction) {
             case 'UP':
-                this.assignCoords({...this.coords, y: this.coords.y + 30})
+                newCoords = {...this.coords, y: Math.max(this.coords.y - 30, 0) }
+
+                this.coords = newCoords;
+                   
+                this.element.style.left = `${newCoords.x}px`;
+                this.element.style.top = `${newCoords.y}px`;
                 return;
             case 'RIGHT':
+                newCoords = {...this.coords, x: Math.max(this.coords.x - 30, 0) }
+
+                this.coords = newCoords;
+                   
+                this.element.style.left = `${newCoords.x}px`;
+                this.element.style.top = `${newCoords.y}px`;
                 return;
             case 'DOWN':
+                newCoords = {...this.coords, y: Math.min(this.coords.y + 30, 600) }
+
+                this.coords = newCoords;
+                   
+                this.element.style.left = `${newCoords.x}px`;
+                this.element.style.top = `${newCoords.y}px`;
                 return;
             case 'LEFT':
+                newCoords = {...this.coords, x: Math.min(this.coords.x + 30, 600) }
+
+                this.coords = newCoords;
+                   
+                this.element.style.left = `${newCoords.x}px`;
+                this.element.style.top = `${newCoords.y}px`;
                 return;
         }
     }
