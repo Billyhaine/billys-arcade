234c403de0e86d1f13db200b9aa9477f52d2e16f
Sam Wallington <samuel.wallingotn@gmail.com>
2024-03-22 11:11:03 +1300

    Add in several more features to the game
    - Proper addition of segments onto the snake when colliding with the food
    - A reset function
    - Movement that chains along the snake
    - Movement that can be changes using either WASD or arrow keys
    - Various helper fn's on the Segment class that facilitate the above

--- index.js
+++ index.js
@@ -7,14 +7,18 @@ const board = document.getElementById('game-board');
 let HEAD;
 let FOOD;
 
-// the coordinates of the food, stored to avoid placing things ontop of eachother
-let FOOD_COORDS = {
+// separating the default coordinates of the food so that we can reset the game
+const DEFAULT_FOOD_COORDS = {
     x: 450,
-    y: 300,
+    y: 300
 };
 
-let DIRECTION = 'LEFT';
+// the coordinates of the food, stored to avoid placing things ontop of eachother
+let FOOD_COORDS = DEFAULT_FOOD_COORDS;
 
+let DIRECTION = 'RIGHT';
+
+// get a random set of coordinates, used to place the food after being eaten.
 const getRandomCoords = () => {
     let x = Math.floor(Math.random() * 20) * 30;
     let y = Math.floor(Math.random() * 20) * 30;
@@ -25,12 +29,13 @@ const getRandomCoords = () => {
     }
 }
 
+// initialises the game board.
 const initialise = () => {
     const food = document.createElement('div');
     food.className = 'food';
 
-    food.style.left = `${FOOD_COORDS.x}px`;
-    food.style.top = `${FOOD_COORDS.y}px`;
+    food.style.left = `${DEFAULT_FOOD_COORDS.x}px`;
+    food.style.top = `${DEFAULT_FOOD_COORDS.y}px`;
 
     FOOD = food;
 
@@ -42,6 +47,49 @@ const initialise = () => {
     board.appendChild(food);
 }
 
+// resets the game back to the default state.
+const resetGame = () => {
+    console.log('resetting');
+
+    // remove 
+    HEAD.reset();
+    FOOD.remove();
+
+    FOOD_COORDS = DEFAULT_FOOD_COORDS;
+    DIRECTION = "RIGHT";
+
+    initialise();
+}
+
+// the event listener function for the `keydown` event.
+const keyListener = (e) => {
+    const key = e.key;
+    
+    switch (key) {
+        // change direction to 'UP'
+        case 'ArrowUp':
+        case 'w':
+            if(DIRECTION !== 'DOWN') DIRECTION = 'UP';
+            return;
+        // change direction to 'RIGHT'
+        case 'd' || 'ArrowRight':
+            if(DIRECTION !== 'LEFT') DIRECTION = 'RIGHT';
+            return;
+        // change direction to 'DOWN'
+        case 's' || 'ArrowDown':
+            if(DIRECTION !== 'UP') DIRECTION = 'DOWN';
+            return;
+        // change direction to 'LEFT'
+        case 'a' || 'ArrowLeft':
+            if(DIRECTION !== 'RIGHT') DIRECTION = 'LEFT';
+            return;
+        // reset the game back to the default state
+        case 'Enter':
+            resetGame();
+            return;
+    }
+}
+
 const tick = () => {
     // snake movement
     HEAD.move(DIRECTION);
@@ -50,18 +98,24 @@ const tick = () => {
         // firstly we move the food
         let NEW_COORDS = getRandomCoords();
 
-        while(HEAD.coords.x === NEW_COORDS.x && HEAD.coords.y === NEW_COORDS.y){
-            console.log('they match')
+        while(HEAD.isOverlapping(NEW_COORDS)){
             NEW_COORDS = getRandomCoords();
         }
 
+        FOOD_COORDS = NEW_COORDS;
+
         FOOD.style.left = `${NEW_COORDS.x}px`;
         FOOD.style.top = `${NEW_COORDS.y}px`;
+
+        // secondly we spawn a new segment on the snake.
+        HEAD.addSegment(DIRECTION, board)
     };
 }
 
 // around 60fps
-setInterval(tick, 250)
+setInterval(tick, 175)
+
+document.addEventListener('keydown', keyListener)
 
 document.addEventListener('DOMContentLoaded', initialise);
 
--- segment.js
+++ segment.js
@@ -25,6 +25,22 @@ export class Segment {
         this.element = element;
     }
 
+    /**
+     * Used to get the relative direction from this segment to another.
+     */
+    getRelativeDirection(segment){
+        // segment is to the left
+        if(this.coords.x < segment.coords.x) return 'RIGHT';
+
+        // segment is to the right
+        if(this.coords.x > segment.coords.x) return 'LEFT';
+
+        // segment is above
+        if(this.coords.y > segment.coords.y) return 'UP';
+
+        // segment is below
+        if(this.coords.y < segment.coords.y) return 'DOWN';
+    }
     /*
      * A simple check as to whether or not this Segment is the tail of the snake.
      */
@@ -35,59 +51,115 @@ export class Segment {
     /**
      * A recursive method used to add the end of the snake.
      * 
-     * @param {Coords} previousCoords the segment that preceeds this one, used to determine position of new segment.
+     * @param {Direction} direction the direction we would want to add the new segment in.
      * @param board the game board for this.
      */
-    addSegment(previousCoords, board){
-        if(!this.isTailSegment){
-            this.child.addSegment(this.coords);
+    addSegment(direction, board){
+        // if it isn't the tail figure out the desired direction & pass it down
+        if(!this.isTailSegment()){
+            this.child.addSegment(this.getRelativeDirection(this.child), board);
         } else {
-            newTail = new Segment({  }, board)
+            let TAIL_COORDS;
+
+            switch (direction) {
+                case 'UP':
+                // one space below
+                TAIL_COORDS = {...this.coords, y: this.coords.y + 30 }
+                break;
+            case 'RIGHT':
+                // one space to the left
+                TAIL_COORDS = {...this.coords, x: this.coords.x - 30 }
+                break;
+            case 'DOWN':
+                // one space above
+                TAIL_COORDS = {...this.coords, y: this.coords.y - 30 }
+                break;
+            case 'LEFT':
+                // one space to the right
+                TAIL_COORDS = {...this.coords, x: this.coords.x + 30 }
+                break;
+            }
+
+            const TAIL = new Segment(TAIL_COORDS, board, this);
+
+            this.child = TAIL;
+
+            if(this.parent) this.element.className = 'segment';
         }
+    }
+
+    isOverlapping(targetCoords){
+        // is the target coordinates actively overlapping this segment
+        const isOverlappingSelf = this.coords.x === targetCoords.x && this.coords.y === targetCoords.y
+
+        // if this is the tail simply return whether it's overlapping itself
+        if(this.isTailSegment()){
+            return isOverlappingSelf;
+        }
+
+        // if we're not the tail return is it overlapping this segment or it's child
+        return isOverlappingSelf || this.child.isOverlapping(targetCoords);
     }
 
     /**
      * A method used to move the snake, recursively applying to it's successive segments
      * 
-     * @param direction One of 'UP', 'RIGHT', 'LEFT', 'DOWN'
+     * @param direction One of 'UP', 'RIGHT', 'LEFT', 'DOWN', NOTE: if this isnt the head this won't be passed.
      * @returns Moves this particular segment in the desired direction & triggers successive moves
      */
-    move(direction){
-        let newCoords
+    move(dir){
+        let newCoords;
+        let direction = dir;
+
+        if(this.parent){
+            direction = this.getRelativeDirection(this.parent);
+        }
 
         switch (direction) {
             case 'UP':
                 newCoords = {...this.coords, y: Math.max(this.coords.y - 30, 0) }
-
-                this.coords = newCoords;
                    
                 this.element.style.left = `${newCoords.x}px`;
                 this.element.style.top = `${newCoords.y}px`;
-                return;
-            case 'RIGHT':
-                newCoords = {...this.coords, x: Math.max(this.coords.x - 30, 0) }
 
-                this.coords = newCoords;
-                   
-                this.element.style.left = `${newCoords.x}px`;
-                this.element.style.top = `${newCoords.y}px`;
-                return;
+                break;
             case 'DOWN':
                 newCoords = {...this.coords, y: Math.min(this.coords.y + 30, 600) }
-
-                this.coords = newCoords;
-                   
+                    
                 this.element.style.left = `${newCoords.x}px`;
                 this.element.style.top = `${newCoords.y}px`;
-                return;
+                break;
             case 'LEFT':
+                newCoords = {...this.coords, x: Math.max(this.coords.x - 30, 0) }
+                    
+                this.element.style.left = `${newCoords.x}px`;
+                this.element.style.top = `${newCoords.y}px`;
+                break;
+            case 'RIGHT':
                 newCoords = {...this.coords, x: Math.min(this.coords.x + 30, 600) }
-
-                this.coords = newCoords;
                    
                 this.element.style.left = `${newCoords.x}px`;
                 this.element.style.top = `${newCoords.y}px`;
-                return;
+                break;   
         }
+
+        if(!this.isTailSegment()){
+            this.child.move();
+        }
+
+        this.coords = newCoords;
     }
+
+    /**
+     * Deletes all childen & then itself
+     */
+    reset(){
+        if(this.child){
+            this.child.reset();
+            this.child = undefined;
+        }
+
+        this.element.remove();
+    }
+
 }
